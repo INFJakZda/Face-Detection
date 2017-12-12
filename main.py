297a81815bf5e5650defff2dda4cadc5478cf305
eaglx <tango_delta@outlook.com>
2017-12-12 17:18:02 +0100

    Update project, Project is closed

--- main.py
+++ main.py
@@ -112,73 +112,8 @@ def detect_eye(image, cords):   #na całym obrazku rysuje obrys oczu dla kazdej 
     image2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     
     image[y:y+w, x:x+h] = cv2.drawContours(image[y:y+w, x:x+h], contours, -1, (100,255,100), 1)
-
-    #cv2.imshow("image",  image)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-    
     return image     
 
-def hand_seek(image):
-    cv2.rectangle(image, (300,300), (100,100), (255,243,58),0)
-    crop_img = image[100:300, 100:300]
-    
-    img_grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(img_grey, (35,35), 0)
-    
-    # thresholdin: Otsu's Binarization method
-    _, thresh1 = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    image2, contours, hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    
-    # find contour with max area
-    cnt = max(contours, key = lambda x: cv2.contourArea(x))
-
-    # create bounding rectangle around the contour (can skip below two lines)
-    x, y, w, h = cv2.boundingRect(cnt)
-    cv2.rectangle(crop_img, (x, y), (x+w, y+h), (0, 0, 255), 0)
-
-    # finding convex hull
-    hull = cv2.convexHull(cnt)
-
-    # drawing contours
-    drawing = np.zeros(crop_img.shape,np.uint8)
-    cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
-    cv2.drawContours(drawing, [hull], 0,(0, 0, 255), 0)
-
-    # finding convex hull
-    hull = cv2.convexHull(cnt, returnPoints=False)
-
-    # finding convexity defects
-    defects = cv2.convexityDefects(cnt, hull)
-    count_defects = 0
-    cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
-    
-    for i in range(defects.shape[0]):
-        s,e,f,d = defects[i,0]
-
-        start = tuple(cnt[s][0])
-        end = tuple(cnt[e][0])
-        far = tuple(cnt[f][0])
-
-        # find length of all sides of triangle
-        a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
-        b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
-        c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
-
-        # apply cosine rule here
-        angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
-
-        # ignore angles > 90 and highlight rest with red dots
-        if angle <= 90:
-            count_defects += 1
-            cv2.circle(crop_img, far, 1, [0,0,255], -1)
-        #dist = cv2.pointPolygonTest(cnt,far,True)
-
-        # draw a line from start to end i.e. the convex points (finger tips)
-        # (can skip this part)
-        cv2.line(crop_img,start, end, [0,255,0], 2)
-    
-    return image
 
 if __name__ == '__main__':
     print("Preparing data...")
@@ -211,8 +146,6 @@ if __name__ == '__main__':
         if(faces != None):
             for it in range(len(faces)):
                 test_img = detect_eye(test_img, cords[it])
-        elif(faces == None):
-            hand_seek(test_img)
         
         test_img = cv2.cvtColor(np.array(test_img), cv2.COLOR_BGR2RGB)
         
